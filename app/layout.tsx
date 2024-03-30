6c6b06ba7c2a20c8f012af079fe278a59ea5d7da
GreatRSingh <rakshitsingh421@gmail.com>
2024-03-30 15:37:08 +0530

    credits

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   const footer = (
     <footer>
       <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
-        <h3>Designed by Pixegami</h3>
+        <h3>Designed by Pixegami, Swadhin, Rakshit</h3>
       </div>
     </footer>
   );
