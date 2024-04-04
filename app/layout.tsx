682f4762de5e9dbe4be2a757aa2734618a44c1b7
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-04-04 21:44:40 +0530

    Added in footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import "../styles/globals.css";
+import SubscribeButton from "../components/Subscribe";
 
 export default function RootLayout({
   children,
@@ -10,7 +11,9 @@ export default function RootLayout({
     <header>
       <div className="text-center p-8 my-6 rounded-md">
         <Link href="/">
-          <h1 className="text-2xl text-slate-700 font-bold mt-4">Rakshit's Blog</h1>
+          <h1 className="text-2xl text-slate-700 font-bold mt-4">
+            Rakshit's Blog
+          </h1>
         </Link>
         <p className="text-slate-400">🤟 Welcome to my tech blog. 💻</p>
       </div>
@@ -21,6 +24,9 @@ export default function RootLayout({
     <footer>
       <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
         <h3>Designed by Pixegami, Swadhin, Rakshit</h3>
+        <div className="flex justify-end">
+          <SubscribeButton />
+        </div>
       </div>
     </footer>
   );
